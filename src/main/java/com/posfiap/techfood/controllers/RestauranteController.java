341b3e14a08cd60459fe47403e10fcf3ead142c1
Winkler30 <charleswinkler76@gmail.com>
2025-05-31 14:24:50 -0300

    feat: adiciona configuração do Swagger

--- src/main/java/com/posfiap/techfood/controllers/RestauranteController.java
+++ src/main/java/com/posfiap/techfood/controllers/RestauranteController.java
@@ -2,6 +2,8 @@ package com.posfiap.techfood.controllers;
 
 import com.posfiap.techfood.models.Restaurante;
 import com.posfiap.techfood.services.RestauranteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,46 +15,46 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/restaurantes")
+@Tag(name = "Restaurantes", description = "Operações relacionadas aos restaurantes")
 public class RestauranteController {
 
     private final RestauranteService restauranteService;
 
+    @Operation(summary = "Listar todos os restaurantes com paginação")
     @GetMapping
-    public ResponseEntity<List<Restaurante>> findAllRestaurantes(@RequestParam("page") int page,
-                                                                 @RequestParam("size") int size) {
+    public ResponseEntity<List<Restaurante>> findAllRestaurantes(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size) {
         var restaurantes = restauranteService.findAllRestaurantes(page, size);
         return ResponseEntity.ok(restaurantes);
     }
 
+    @Operation(summary = "Buscar restaurante por ID")
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Restaurante>> findRestauranteById(@PathVariable("id") Long id){
         var restaurante = restauranteService.findRestauranteById(id);
         return ResponseEntity.ok(restaurante);
     }
+
+    @Operation(summary = "Cadastrar novo restaurante")
     @PostMapping
-    public ResponseEntity<Void> inserirRestaurante(
-        @RequestBody Restaurante restaurante
-        ){
+    public ResponseEntity<Void> inserirRestaurante(@RequestBody Restaurante restaurante){
         restauranteService.insertRestaurante(restaurante);
         return ResponseEntity.status(201).build();
     }
 
+    @Operation(summary = "Atualizar restaurante existente")
     @PutMapping("/{id}")
-    public ResponseEntity<Void> atualizarRestaurante(
-            @PathVariable("id") Long id, @RequestBody Restaurante restaurante
-    ){
+    public ResponseEntity<Void> atualizarRestaurante(@PathVariable("id") Long id,
+                                                     @RequestBody Restaurante restaurante){
         restauranteService.updateRestaurante(restaurante, id);
-        var status = HttpStatus.NO_CONTENT;
-        return ResponseEntity.status(status.value()).build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Excluir restaurante")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluirRestaurante(
-            @PathVariable("id") Long id){
+    public ResponseEntity<Void> excluirRestaurante(@PathVariable("id") Long id){
         restauranteService.deleteRestaurante(id);
         return ResponseEntity.ok().build();
     }
-
-
 }
-
